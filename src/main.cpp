ce0b3318eaa37f86817b8e26616bcb2a84f0bcf5
unknown <luke.g@posteo.de>
2026-03-14 15:26:16 +0100

    Mit Wlan verbunden und Verbindung auf Terminal angezeigt

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,44 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <time.h>
 
+//Wifi
+const char* name = "BLLLM";
+const char* password = "Schanzenshop100";
+
+//Pins
 int latch = 16;
 int clockPin = 17;
 int dataSerial = 18;
 int powerPin = 19;
 
+//Debounce + Schaltintervalle
 unsigned long letzteLichtZeit = 0;
 unsigned long letzteTasterZeit = 0;
 int lichtIntervall = 200;
 int tasterIntervall = 30;
 
+//Variablen
 bool speicher = false;
 bool anSignal = true;
 int counter = 0;
 
 void setup() {
+  Serial.begin(115200);
+  WiFi.begin(name, password);
   pinMode(latch, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataSerial, OUTPUT);
   pinMode(powerPin, INPUT_PULLUP);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("WLAN verbunden");
 }
 
 void loop() {
+
   unsigned long jetzt = millis();
   bool tasterGedrueckt = !digitalRead(powerPin);
 
